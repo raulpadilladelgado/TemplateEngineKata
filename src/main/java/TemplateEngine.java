f86555554972195111094596854129908a88eac1
raulpadilladelgado <rpd27@outlook.com>
2020-06-09 08:15:34 +0100

    extract parse method to a class

--- src/main/java/TemplateEngine.java
+++ src/main/java/TemplateEngine.java
@@ -1,2 +1,19 @@
+import java.util.Map;
+
 public class TemplateEngine {
+    static String parse(String template, Map<String, String> variables) {
+        if(template==null){
+            return "";
+        }
+        if(variables==null){
+            return template;
+        }
+        for (String key : variables.keySet()) {
+            String value = variables.get(key);
+            if (value != null) {
+                template = template.replace("`$" + key + "`", value);
+            }
+        }
+        return template;
+    }
 }
--- src/test/java/TemplateEngineShould.java
+++ src/test/java/TemplateEngineShould.java
@@ -26,46 +26,31 @@ public class TemplateEngineShould {
      */
     @Test
     void produce_an_empty_template_when_there_is_no_template(){
-        assertThat(parse(null, new HashMap<>())).isEqualTo("");
+        assertThat(TemplateEngine.parse(null, new HashMap<>())).isEqualTo("");
     }
 
     @Test
     void produce_the_same_template_when_there_are_no_variables() {
-        assertThat(parse("hola", new HashMap<>())).isEqualTo("hola");
-        assertThat(parse("hola", null)).isEqualTo("hola");
+        assertThat(TemplateEngine.parse("hola", new HashMap<>())).isEqualTo("hola");
+        assertThat(TemplateEngine.parse("hola", null)).isEqualTo("hola");
     }
 
     @Test
     void produce_the_same_template_when_variables_in_template_have_incorrect_format(){
         Map<String, String> variables = new HashMap<>();
         variables.put("user", "carlos");
-        assertThat(parse("hola user", variables)).isEqualTo("hola user");
+        assertThat(TemplateEngine.parse("hola user", variables)).isEqualTo("hola user");
     }
 
     @Test
     void parse_any_variables_in_template() {
         Map<String, String> variables = new HashMap<>();
         variables.put("user", "carlos");
-        assertThat(parse("hola `$user`", variables)).isEqualTo("hola carlos");
+        assertThat(TemplateEngine.parse("hola `$user`", variables)).isEqualTo("hola carlos");
         variables.put("day", "lunes");
-        assertThat(parse("hola `$user`, hoy es dia `$day`", variables)).isEqualTo("hola carlos, hoy es dia lunes");
+        assertThat(TemplateEngine.parse("hola `$user`, hoy es dia `$day`", variables)).isEqualTo("hola carlos, hoy es dia lunes");
     }
 
 
-    private String parse(String template, Map<String, String> variables) {
-        if(template==null){
-            return "";
-        }
-        if(variables==null){
-            return template;
-        }
-        for (String key : variables.keySet()) {
-            String value = variables.get(key);
-            if (value != null) {
-                template = template.replace("`$" + key + "`", value);
-            }
-        }
-        return template;
-    }
 }
 
